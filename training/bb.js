4ddfcee5946967671d35525dec7d74a06a941cef
cjwinchester <cjwinchester@gmail.com>
2015-01-27 09:53:47 -0600

    training landing page

---
+++ training/bb.js
@@ -0,0 +1,86 @@
+var bb = [
+    {
+        'name': 'Advanced video training',
+        'date': new Date("February 11, 2015 12:30:00"),
+        'descrip': 'In this session, you\'ll learn how to shoot iPhone video like a boss.',
+        'tut': 'https://github.com/OWH-projects/video-training',
+        'dl': 'https://github.com/OWH-projects/video-training/archive/master.zip'
+    },
+    {
+        'name': 'Basic HTML/CSS',
+        'date': new Date("February 18, 2015 12:30:00"),
+        'descrip': 'An introduction to making things for the web',
+        'tut': 'https://github.com/OWH-projects/html-css-training',
+        'dl': 'https://github.com/OWH-projects/html-css-training/archive/master.zip'
+    },
+    {
+        'name': 'Basic Javascript',
+        'date': new Date("February 25, 2015 12:30:00"),
+        'descrip': 'If HTML = nouns and CSS = adjectives, Javascript = verbs. In this session, we\'ll learn some basic syntax of the web\'s scripting language.',
+        'tut': 'https://github.com/OWH-projects/javascript-training',
+        'dl': 'https://github.com/OWH-projects/javascript-training/archive/master.zip'
+    },
+    {
+        'name': 'Using Twitter like a pro',
+        'date': new Date("March 4, 2015 12:30:00"),
+        'descrip': 'Twitter is where the action is. Learn to use it to find story leads and connect with readers.',
+        'tut': 'https://github.com/OWH-projects/twitter-training',
+        'dl': 'https://github.com/OWH-projects/twitter-training/archive/master.zip'
+    },
+    {
+        'name': 'Excel and Google Spreadsheets',
+        'date': new Date("March 11, 2015 12:30:00"),
+        'descrip': 'Search, sort and sift data to add quantitative heft to your reporting. If you don\'t have a copy of Excel, you\'ll need a <a href="http://drive.google.com">Google account</a>.',
+        'tut': 'https://github.com/OWH-projects/excel-training',
+        'dl': 'https://github.com/OWH-projects/excel-training/archive/master.zip'
+    },
+    {
+        'name': 'Introduction to SQL',
+        'date': new Date("March 18, 2015 12:30:00"),
+        'descrip': 'If you have more data than Excel can handle, Structured Query Language is your friend. In this session we\'ll be using a browser add-on for <a href="https://www.mozilla.org/en-US/firefox/new/">Firefox</a> called <a href="https://addons.mozilla.org/en-US/firefox/addon/sqlite-manager/">SQLite Manager</a>.',
+        'tut': 'https://github.com/OWH-projects/sql-training',
+        'dl': 'https://github.com/OWH-projects/sql-training/archive/master.zip'
+    },
+    {
+        'name': 'Social media research',
+        'date': new Date("March 25, 2015 12:30:00"),
+        'descrip': 'Tips and tricks for mining social media sites for information about people.',
+        'tut': 'https://github.com/OWH-projects/social-media-training',
+        'dl': 'https://github.com/OWH-projects/social-media-training/archive/master.zip'
+    },
+    {
+        'name': 'Chartbeat',
+        'date': new Date("March 25, 2015 12:30:00"),
+        'descrip': 'An introduction to web analytics, and why you should care about how readers use our website.',
+        'tut': 'https://github.com/OWH-projects/chartbeat-training',
+        'dl': 'https://github.com/OWH-projects/chartbeat-training/archive/master.zip'
+    },
+    {
+        'name': 'Making a "Big Story"',
+        'date': new Date("April 8, 2015 12:30:00"),
+        'descrip': 'Got a big story? Use this tool to blow it up proper for the web.',
+        'tut': 'https://github.com/OWH-projects/big-story-training',
+        'dl': 'https://github.com/OWH-projects/big-story-training/archive/master.zip'
+    },
+    {
+        'name': 'Introduction to Python, part 1',
+        'date': new Date("April 15, 2015 12:30:00"),
+        'descrip': 'Python is a programming language that can help automate reporting tasks, scrape data from the web, bake cakes, travel through time ... and more! This is a basic overview. We\'ll be using <a href="https://www.python.org/download/releases/2.7/">Python 2.7</a>, so make sure you have that installed.',
+        'tut': 'https://github.com/OWH-projects/python-training',
+        'dl': 'https://github.com/OWH-projects/python-training/archive/master.zip'
+    },
+    {
+        'name': 'Introduction to Python, part 2',
+        'date': new Date("April 22, 2015 12:30:00"),
+        'descrip': 'A hands-on follow-up to the introductory session. We\'ll go over basic data structures in Python and scrape a simple web page.',
+        'tut': 'https://github.com/OWH-projects/python-training',
+        'dl': 'https://github.com/OWH-projects/python-training/archive/master.zip'
+    },
+    {
+        'name': 'Interactive maps',
+        'date': new Date("April 29, 2015 12:30:00"),
+        'descrip': 'OK so you have a bunch of addresses that you want to plot on an interactive map. This session will get you started. Make sure you have a <a href="http://drive.google.com">Google</a> account.',
+        'tut': 'https://github.com/OWH-projects/maps-training',
+        'dl': 'https://github.com/OWH-projects/maps-training/archive/master.zip'
+    }
+]
