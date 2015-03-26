628190bad73580acc47c74ad5c736333967050ca
cjwinchester <cjwinchester@gmail.com>
2015-03-26 02:14:48 -0500

    sewer table

---
+++ sandbox/tables/js/sewers.js
@@ -0,0 +1,33 @@
+var sewers = [
+ {
+  'name': 'MUD users',
+  'frequency': 'monthly',
+  'table': [{"z":"Residential customer charge","e2015":"$8.42 ","e2016":"$8.82 ","e2017":"$9.24 ","e2018":"$9.73 "},
+{"z":"Commercial customer charge","e2015":"$8.42 ","e2016":"$8.82 ","e2017":"$9.24 ","e2018":"$9.73 "},
+{"z":"Residential flow charge (per CCF)","e2015":"$1.76 ","e2016":"$1.92 ","e2017":"$2.11 ","e2018":"$2.30 "},
+{"z":"Commercial flow charge (per CCF)","e2015":"$1.65 ","e2016":"$1.81 ","e2017":"$2.00 ","e2018":"$2.19 "}]
+ }, 
+   {
+  'name': 'B-4 Corners SID',
+  'frequency': 'quarterly',
+  'table': [{"z":"Customer charge per user","e2015":"$27.75 ","e2016":"$29.19 ","e2017":"$30.69 ","e2018":"$32.49 "},
+{"z":"Flow charge (per CCF)","e2015":"$1.65 ","e2016":"$1.81 ","e2017":"$2.00 ","e2018":"$2.19 "},
+{"z":"Abnormal biochemical oxygen demand charge / total nitrogen","e2015":"$215.96 ","e2016":"$226.93 ","e2017":"$238.01 ","e2018":"$250.83 "},
+{"z":"Abnormal suspended solids charge / total nitrogen","e2015":"$340.53 ","e2016":"$357.69 ","e2017":"$374.84 ","e2018":"$394.68 "}] 
+  },
+  {
+  'name': 'Westmont SID',
+  'frequency': 'monthly',
+  'table': [{"z":"Customer charge","e2015":"$7.04 ","e2016":"$7.41 ","e2017":"$7.78 ","e2018":"$8.24 "},
+{"z":"Flow charge (per CCF)","e2015":"$1.76 ","e2016":"$1.92 ","e2017":"$2.11 ","e2018":"$2.30 "},
+{"z":"Hand charge","e2015":"$10.42 ","e2016":"$10.74 ","e2017":"$11.06 ","e2018":"$11.39 "},
+{"z":"Abnormal biochemical oxygen demand charge / total nitrogen","e2015":"$215.96 ","e2016":"$226.93 ","e2017":"$238.01 ","e2018":"$250.83 "},
+{"z":"Abnormal suspended solids charge / total nitrogen","e2015":"$340.53 ","e2016":"$357.69 ","e2017":"$374.84 ","e2018":"$394.68 "}]    
+  },
+  {
+  'name': 'Flat-fee customers',
+  'frequency': 'annually',
+  'table': [{"z":"City of Gretna","e2015":"$23,050 ","e2016":"$23,550 ","e2017":"$24,050 ","e2018":"$24,550 "},
+{"z":"Tiburon","e2015":"$2,400 ","e2016":"$2,450 ","e2017":"$2,500 ","e2018":"$2,550 "}]
+    }
+]
