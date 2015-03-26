493e057a46f89481040e07983f22ed92dfc33ec1
cjwinchester <cjwinchester@gmail.com>
2015-03-26 18:05:22 -0500

    fix

--- sandbox/tables/js/sewers.js
+++ sandbox/tables/js/sewers.js
@@ -12,8 +12,8 @@ var sewers = [
   'frequency': 'quarterly',
   'table': [{"z":"Customer charge per user","e2015":"$27.75 ","e2016":"$29.19 ","e2017":"$30.69 ","e2018":"$32.49 "},
 {"z":"Flow charge (per CCF)","e2015":"$1.65 ","e2016":"$1.81 ","e2017":"$2.00 ","e2018":"$2.19 "},
-{"z":"Abnormal biochemical oxygen demand charge / total nitrogen","e2015":"$215.96 ","e2016":"$226.93 ","e2017":"$238.01 ","e2018":"$250.83 "},
-{"z":"Abnormal suspended solids charge / total nitrogen","e2015":"$340.53 ","e2016":"$357.69 ","e2017":"$374.84 ","e2018":"$394.68 "}] 
+{"z":"Abnormal biochemical oxygen demand charge / ton","e2015":"$215.96 ","e2016":"$226.93 ","e2017":"$238.01 ","e2018":"$250.83 "},
+{"z":"Abnormal suspended solids charge / ton","e2015":"$340.53 ","e2016":"$357.69 ","e2017":"$374.84 ","e2018":"$394.68 "}] 
   },
   {
   'name': 'Westmont SID',
@@ -21,8 +21,8 @@ var sewers = [
   'table': [{"z":"Customer charge","e2015":"$7.04 ","e2016":"$7.41 ","e2017":"$7.78 ","e2018":"$8.24 "},
 {"z":"Flow charge (per CCF)","e2015":"$1.76 ","e2016":"$1.92 ","e2017":"$2.11 ","e2018":"$2.30 "},
 {"z":"Hand charge","e2015":"$10.42 ","e2016":"$10.74 ","e2017":"$11.06 ","e2018":"$11.39 "},
-{"z":"Abnormal biochemical oxygen demand charge / total nitrogen","e2015":"$215.96 ","e2016":"$226.93 ","e2017":"$238.01 ","e2018":"$250.83 "},
-{"z":"Abnormal suspended solids charge / total nitrogen","e2015":"$340.53 ","e2016":"$357.69 ","e2017":"$374.84 ","e2018":"$394.68 "}]    
+{"z":"Abnormal biochemical oxygen demand charge / ton","e2015":"$215.96 ","e2016":"$226.93 ","e2017":"$238.01 ","e2018":"$250.83 "},
+{"z":"Abnormal suspended solids charge / ton","e2015":"$340.53 ","e2016":"$357.69 ","e2017":"$374.84 ","e2018":"$394.68 "}]    
   },
   {
   'name': 'Flat-fee customers',
