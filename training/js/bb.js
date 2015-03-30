6e6fab0c235e6c62c95e19dde82df1560224b2e8
Cody Winchester <cjwinchester@gmail.com>
2015-03-30 10:47:51 -0500

    added data literacy

--- training/js/bb.js
+++ training/js/bb.js
@@ -1,87 +1,94 @@
 var bb = [
     {
-        name: 'Advanced video training',
-        date: "February 18, 2015 13:00:00",
-        descrip: 'In this session, you\'ll learn how to shoot iPhone video like a boss. We won\'t be editing. Instead, we\'ll spend time looking at what\'s working and what\'s not on Omaha.com. We\'re also going to look at ways to make your videos better with new editing techniques and shooting tips.',
-        tut: 'https://github.com/OWH-projects/video-training'
+        "name": 'Advanced video training',
+        "date": "February 18, 2015 13:00:00",
+        "descrip": 'In this session, you\'ll learn how to shoot iPhone video like a boss. We won\'t be editing. Instead, we\'ll spend time looking at what\'s working and what\'s not on Omaha.com. We\'re also going to look at ways to make your videos better with new editing techniques and shooting tips.',
+        "tut": 'https://github.com/OWH-projects/video-training'
     },
     {
-        name: 'Basic HTML/CSS',
-        date: "February 25, 2015 13:00:00",
-        descrip: 'An introduction to making things for the web. If you can, bring a laptop with a good text editor installed. Suggestions: <a href="http://notepad-plus-plus.org" target="_blank">notepad++</a> for Windows, <a href="http://www.sublimetext.com" target="_blank">Sublime</a> or <a href="http://www.barebones.com/products/textwrangler/" target="_blank">TextWrangler</a> for Macs. (If you\'re bumping Linux, presumably you\'ve already got a good text editor installed.)',
-        tut: 'https://github.com/OWH-projects/html-css-training',
-        dl: 'https://github.com/OWH-projects/html-css-training/archive/master.zip'
+        "name": 'Basic HTML/CSS',
+        "date": "February 25, 2015 13:00:00",
+        "descrip": 'An introduction to making things for the web. If you can, bring a laptop with a good text editor installed. Suggestions: <a href="http://notepad-plus-plus.org" target="_blank">notepad++</a> for Windows, <a href="http://www.sublimetext.com" target="_blank">Sublime</a> or <a href="http://www.barebones.com/products/textwrangler/" target="_blank">TextWrangler</a> for Macs. (If you\'re bumping Linux, presumably you\'ve already got a good text editor installed.)',
+        "tut": 'https://github.com/OWH-projects/html-css-training',
+        "dl": 'https://github.com/OWH-projects/html-css-training/archive/master.zip'
     },
     {
-        name: 'Basic Javascript',
-        date: "March 4, 2015 13:00:00",
-        descrip: 'If HTML tags are the nouns of the web and CSS rules are adjectives, Javascript functions are the verbs. In this session, we\'ll learn some basic syntax for the scripting language that powers the Internet. You\'ll want to have some familiarity with HTML and CSS before attending this training. Also, it would be a good idea to bring a laptop with a good text editor installed (see above).',
-        tut: 'https://github.com/OWH-projects/javascript-training',
-        dl: 'https://github.com/OWH-projects/javascript-training/archive/master.zip'
+        "name": 'Basic Javascript',
+        "date": "March 4, 2015 13:00:00",
+        "descrip": 'If HTML tags are the nouns of the web and CSS rules are adjectives, Javascript functions are the verbs. In this session, we\'ll learn some basic syntax for the scripting language that powers the Internet. You\'ll want to have some familiarity with HTML and CSS before attending this training. Also, it would be a good idea to bring a laptop with a good text editor installed (see above).',
+        "tut": 'https://github.com/OWH-projects/javascript-training',
+        "dl": 'https://github.com/OWH-projects/javascript-training/archive/master.zip'
     },
     {
-        name: 'Using Twitter like a pro',
-        date: "March 11, 2015 13:00:00",
-        descrip: 'Twitter is where the action is. Learn to use it to find story leads and connect with readers. Make sure you have a <a href="http://www.twitter.com" target="_blank">Twitter account</a> set up and <a href="http://www.google.com/chrome" target="_blank">Google Chrome</a> installed (especially if you want to follow along on your own computer).'
+        "name": 'Using Twitter like a pro',
+        "date": "March 11, 2015 13:00:00",
+        "descrip": 'Twitter is where the action is. Learn to use it to find story leads and connect with readers. Make sure you have a <a href="http://www.twitter.com" target="_blank">Twitter account</a> set up and <a href="http://www.google.com/chrome" target="_blank">Google Chrome</a> installed (especially if you want to follow along on your own computer).'
     },
     {
-        name: 'Spreadsheets',
-        date: "March 18, 2015 13:00:00",
-        descrip: 'Search, sort and sift data to add quantitative heft to your reporting. A laptop would be useful, and you\'ll need a <a href="http://drive.google.com">Google account</a> to follow along.',
-        tut: 'https://github.com/OWH-projects/excel-training'
+        "name": 'Spreadsheets',
+        "date": "March 18, 2015 13:00:00",
+        "descrip": 'Search, sort and sift data to add quantitative heft to your reporting. A laptop would be useful, and you\'ll need a <a href="http://drive.google.com">Google account</a> to follow along.',
+        "tut": 'https://github.com/OWH-projects/excel-training'
     },
     {
-        name: 'Introduction to SQL',
-        date: "March 25, 2015 13:00:00",
-        descrip: 'If you have more data than Excel can handle, Structured Query Language is your friend. In this session we\'ll be using a browser add-on for <a href="https://www.mozilla.org/en-US/firefox/new/" target="_blank">Firefox</a> called <a href="https://addons.mozilla.org/en-US/firefox/addon/sqlite-manager/" target="_blank">SQLite Manager</a>.',
+        "name": 'Introduction to SQL',
+        "date": "March 25, 2015 13:00:00",
+        "descrip": 'If you have more data than Excel can handle, Structured Query Language is your friend. In this session we\'ll be using a browser add-on for <a href="https://www.mozilla.org/en-US/firefox/new/" target="_blank">Firefox</a> called <a href="https://addons.mozilla.org/en-US/firefox/addon/sqlite-manager/" target="_blank">SQLite Manager</a>.',
     },
     {
-        name: 'Social media research',
-        date: "April 1, 2015 13:00:00",
-        descrip: 'Tips and tricks for mining social media sites for information about people.',
-        tut: 'https://github.com/OWH-projects/social-media-training',
-        dl: 'https://github.com/OWH-projects/social-media-training/archive/master.zip'
+        "name": 'Social media research',
+        "date": "April 1, 2015 13:00:00",
+        "descrip": 'Tips and tricks for mining social media sites for information about people.',
+        "tut": 'https://github.com/OWH-projects/social-media-training',
+        "dl": 'https://github.com/OWH-projects/social-media-training/archive/master.zip'
     },
     {
-        name: 'Chartbeat',
-        date: "April 8, 2015 13:00:00",
-        descrip: 'An introduction to web analytics, and why you should care about how readers use Omaha.com.',
-        tut: 'https://github.com/OWH-projects/chartbeat-training',
-        dl: 'https://github.com/OWH-projects/chartbeat-training/archive/master.zip'
+        "name": 'Chartbeat',
+        "date": "April 8, 2015 13:00:00",
+        "descrip": 'An introduction to web analytics, and why you should care about how readers use Omaha.com.',
+        "tut": 'https://github.com/OWH-projects/chartbeat-training',
+        "dl": 'https://github.com/OWH-projects/chartbeat-training/archive/master.zip'
     },
     {
-        name: 'Making a "Big Story"',
-        date: "April 15, 2015 13:00:00",
-        descrip: 'Got a big story? Use this tool to blow it up proper for the web.',
-        tut: 'https://github.com/OWH-projects/big-story-training',
-        dl: 'https://github.com/OWH-projects/big-story-training/archive/master.zip'
+        "name": 'Making a "Big Story"',
+        "date": "April 15, 2015 13:00:00",
+        "descrip": 'Got a big story? Use this tool to blow it up proper for the web.',
+        "tut": 'https://github.com/OWH-projects/big-story-training',
+        "dl": 'https://github.com/OWH-projects/big-story-training/archive/master.zip'
     },
     {
-        name: 'Introduction to Python, part 1',
-        date: "April 22, 2015 13:00:00",
-        descrip: 'Python is a programming language that can help automate reporting tasks, scrape data from the web, bake cakes, travel through time ... and more! This is a basic overview. We\'ll be using <a href="https://www.python.org/download/releases/2.7/">Python 2.7</a>, so make sure you have that and a good text editor installed.',
-        tut: 'https://github.com/OWH-projects/python-training',
-        dl: 'https://github.com/OWH-projects/python-training/archive/master.zip'
+        "name": 'Data literacy',
+        "date": "April 22, 2015 13:00:00",
+        "descrip": 'We\'ll talk about how to find data on your beat, strategies for negotiating with truculent bureaucrats, sanity checks and the gift of paranoia.',
+        "tut": 'https://github.com/OWH-projects/data-literacy',
+        "dl": 'https://github.com/OWH-projects/data-literacy/archive/master.zip'
     },
     {
-        name: 'Introduction to Python, part 2',
-        date: "April 29, 2015 13:00:00",
-        descrip: 'A hands-on follow-up to the introductory session. We\'ll go over basic data structures in Python and scrape a simple web page.',
-        tut: 'https://github.com/OWH-projects/python-training',
-        dl: 'https://github.com/OWH-projects/python-training/archive/master.zip'
+        "name": 'Introduction to Python, part 1',
+        "date": "April 29, 2015 13:00:00",
+        "descrip": 'Python is a programming language that can help automate reporting tasks, scrape data from the web, bake cakes, travel through time ... and more! This is a basic overview. We\'ll be using <a href="https://www.python.org/download/releases/2.7/">Python 2.7</a>, so make sure you have that and a good text editor installed.',
+        "tut": 'https://github.com/OWH-projects/python-training',
+        "dl": 'https://github.com/OWH-projects/python-training/archive/master.zip'
     },
     {
-        name: 'Interactive maps',
-        date: "May 6, 2015 13:00:00",
-        descrip: 'So you have a bunch of addresses you want to plot on an interactive map. This session will get you started. Make sure you have a <a href="http://drive.google.com">Google account</a>.',
-        tut: 'https://github.com/OWH-projects/maps-training',
-        dl: 'https://github.com/OWH-projects/maps-training/archive/master.zip'
+        "name": 'Introduction to Python, part 2',
+        "date": "May 6, 2015 13:00:00",
+        "descrip": 'A hands-on follow-up to the introductory session. We\'ll go over basic data structures in Python and scrape a simple web page.',
+        "tut": 'https://github.com/OWH-projects/python-training',
+        "dl": 'https://github.com/OWH-projects/python-training/archive/master.zip'
     },
     {
-        name: 'Backgrounding',
-        date: "May 13, 2015 13:00:00",
-        descrip: 'Learn how to use web tools to background sources, suspects and significant others. (Kidding!) (But really.) You don\'t need to bring anything except for a notebook.',
-        tut: 'https://github.com/OWH-projects/backgrounding-training',
-        dl: 'https://github.com/OWH-projects/backgrounding-training/archive/master.zip'
+        "name": 'Interactive maps',
+        "date": "May 13, 2015 13:00:00",
+        "descrip": 'So you have a bunch of addresses you want to plot on an interactive map. This session will get you started. Make sure you have a <a href="http://drive.google.com">Google account</a>.',
+        "tut": 'https://github.com/OWH-projects/maps-training',
+        "dl": 'https://github.com/OWH-projects/maps-training/archive/master.zip'
+    },
+    {
+        "name": 'Backgrounding',
+        "date": "May 20, 2015 13:00:00",
+        "descrip": 'Learn how to use web tools to background sources, suspects and significant others. (Kidding!) (But really.) You don\'t need to bring anything except for a notebook.',
+        "tut": 'https://github.com/OWH-projects/backgrounding-training',
+        "dl": 'https://github.com/OWH-projects/backgrounding-training/archive/master.zip'
     }
 ]
